a75e45ac36e227c11b9ef0b5f20362cca6f9d432
Pallavi <pallavi.p8296@gmail.com>
2019-06-18 10:33:06 +0530

    Kafka-Producer-Consumer POC for passing string

---
+++ kafka-consumer/src/main/java/com/stackroute/kafkaconsumer/domain/Consumer.java
@@ -0,0 +1,27 @@
+package com.stackroute.kafkaconsumer.domain;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+
+import java.util.concurrent.CountDownLatch;
+
+public class Consumer {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(Consumer.class);
+
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    public CountDownLatch getLatch() {
+        return latch;
+    }
+
+    @KafkaListener(topics = "helloworld.t")
+    public void receive(String payload) {
+        LOGGER.info("received payload='{}'", payload);
+        System.out.println(payload + "  this is my payload !!!");
+        latch.countDown();
+    }
+
+}
